1e8011e3f6452082b66d2035c993ee263235652a
wildlifehexagon <crossthebreeze@gmail.com>
2017-09-15 21:36:16 -0500

    add delete post feature

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchPost } from '../actions';
+import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
@@ -9,6 +9,14 @@ class PostsShow extends Component {
       const { id } = this.props.match.params;
       this.props.fetchPost(id);
     }
+  }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+
+    this.props.deletePost(id, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
@@ -24,6 +32,7 @@ class PostsShow extends Component {
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
         <Link to="/" className="btn btn-primary">Back to Index</Link>
+        <button className="btn btn-danger pull-xs-right" onClick={this.onDeleteClick.bind(this)}>Delete Post</button>
       </div>
     );
   }
@@ -33,4 +42,4 @@ function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
