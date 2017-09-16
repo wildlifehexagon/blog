2b0c51bb99606a02c8788ef10c32809a9cc24c3d
wildlifehexagon <crossthebreeze@gmail.com>
2017-09-15 20:58:50 -0500

    add FETCH_POST action creator

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,12 +1,17 @@
 import _ from 'lodash';
-import { FETCH_POSTS, CREATE_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
+  case FETCH_POST:
+    // const post = action.payload.data;
+    // const newState = { ...state };
+    // newState[post.id] = post;
+    // return newState;
+
+    return { ...state, [action.payload.data.id]: action.payload.data }
   case FETCH_POSTS:
     return _.mapKeys(action.payload.data, 'id');
-  // case CREATE_POST:
-  //
   default:
     return state;
   }
