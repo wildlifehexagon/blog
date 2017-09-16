a31e962d40852125f9688f4c9c29258cfe49f687
wildlifehexagon <crossthebreeze@gmail.com>
2017-09-15 19:21:01 -0500

    add redux-form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
   render() {
     return (
-      <div>
-        PostsNew
-      </div>
+      <form>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
