8d0ed479fc44ce49fa5542b4c41b4c33674e7d53
wildlifehexagon <crossthebreeze@gmail.com>
2017-09-15 20:46:51 -0500

    finish navigation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
   renderField(field) {
@@ -23,7 +25,9 @@ class PostsNew extends Component {
   }
 
   onSubmit(values) {
-
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
@@ -75,4 +79,6 @@ function validate(values) {
 export default reduxForm({
   validate,
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null, { createPost })(PostsNew)
+);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, CREATE_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
   case FETCH_POSTS:
     return _.mapKeys(action.payload.data, 'id');
+  // case CREATE_POST:
+  //
   default:
     return state;
   }
